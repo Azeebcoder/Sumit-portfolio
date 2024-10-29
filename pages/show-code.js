5259096fdf3d4ba4e451308e6d48e3dc2e545774
Azeeb_coder <djadamnoob9568@gmail.com>
2024-10-29 22:03:18 +0530

    Update show-code.js

--- pages/show-code.js
+++ pages/show-code.js
@@ -12,21 +12,23 @@ window.addEventListener("load", async () => {
         alldata = await fetchData('/details/projects.json');
         const userIdToFind = data;
         const code = alldata.find(code => code.title === userIdToFind);
-        fetch(code.codes)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.text();
-            })
-            .then(data => {
-                const formattedCode = `${escapeHtml(data)}`;
-                content.innerHTML = formattedCode;
-                hljs.highlightAll();
-            })
-    } catch (error) {
-        console.log("error")
-    }
+    //     fetch(code.codes)
+    //         .then(response => {
+    //             if (!response.ok) {
+    //                 throw new Error('Network response was not ok');
+    //             }
+    //             return response.text();
+    //         })
+    //         .then(data => {
+    //             const formattedCode = `${escapeHtml(data)}`;
+    //             content.innerHTML = formattedCode;
+    //             hljs.highlightAll();
+    //         })
+    // } catch (error) {
+    //     console.log("error")
+    // }
+        content.innerHTML = "#include stdio.h";
+        hljs.highlightAll();
 });
 
 async function fetchData(url) {
