d267f200032fed0ac58ef7ea89e4903f3df8961c
Azeeb_coder <djadamnoob9568@gmail.com>
2024-10-30 07:10:26 +0530

    corrected code

--- pages/show-code.js
+++ pages/show-code.js
@@ -9,26 +9,24 @@ const data = getQueryParams();
 window.addEventListener("load", async () => {
 
     try {
-        alldata = await fetchData('/details/projects.json');
+        alldata = await fetchData('../details/projects.json');
         const userIdToFind = data;
         const code = alldata.find(code => code.title === userIdToFind);
-    //     fetch(code.codes)
-    //         .then(response => {
-    //             if (!response.ok) {
-    //                 throw new Error('Network response was not ok');
-    //             }
-    //             return response.text();
-    //         })
-    //         .then(data => {
-    //             const formattedCode = `${escapeHtml(data)}`;
-    //             content.innerHTML = formattedCode;
-    //             hljs.highlightAll();
-    //         })
-    // } catch (error) {
-    //     console.log("error")
-    // }
-        content.innerHTML = "#include stdio.h";
-        hljs.highlightAll();
+        fetch(code.codes)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.text();
+            })
+            .then(data => {
+                const formattedCode = `${escapeHtml(data)}`;
+                content.innerHTML = formattedCode;
+                hljs.highlightAll();
+            })
+    } catch (error) {
+        console.log("error")
+    }
 });
 
 async function fetchData(url) {
